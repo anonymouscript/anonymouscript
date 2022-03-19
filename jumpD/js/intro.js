d511b638716300d7293b0beecf50de51ffaf0b0d
Davis <d.organ@outlook.com>
2022-03-19 08:07:31 -0500

    started a text crawl, must write it better

--- jumpD/js/intro.js
+++ jumpD/js/intro.js
@@ -4,7 +4,11 @@ class FirstScreen extends Phaser.Scene {
 
         this.style = {
             fontFace: "libreBaskerville",
-            fontSize: "75px"
+            fontSize: "75px",
+            wordWrap: {
+                width: 670,
+                advanced: false
+            }
         };
 
     }
@@ -13,6 +17,32 @@ class FirstScreen extends Phaser.Scene {
         this.centerX = config.width / 2;
         this.centerY = config.height / 2;
         var LibreBaskerville = new FontFace("LibreBaskerville", `url(assets/fonts/Libre-Baskerville/LibreBaskerville-Regular.ttf)`)
+    }
+    secondSlide(){
+        
+        this.style.fontSize = "20px"
+        this.out = this.add.text(50,50, "", this.style);
+        this.slide = 1;  
+    }
+    slidethree(){
+        this.slide = 2;
+    }
+    printText(textObj, text, callback, args, scope){
+        if(this.wait && this.wait > 0){
+            this.wait -= 1000/60
+            return text;
+        }
+        if(text[0] == "<"){
+            let dex = text.indexOf(">");
+            let newS = text.slice(1, dex);
+            this.wait += +newS;
+            return text.slice(dex + 1);
+        }
+        textObj.text += text[0];
+        let s = text.slice(1);
+        this.wait += 1000/60
+        return s == "" ? callback.bind(scope)(args) : s;
+
     }
     startPointerDown() {
         this.startTween.resume();
@@ -34,15 +64,25 @@ class FirstScreen extends Phaser.Scene {
             alpha: 0,
             paused: true,
             ease: "power0",
-            onComplete: this.activateSceneMain.bind(this)
-
+            onComplete: this.secondSlide.bind(this)
         });
-
-
+        
+        this.start = start;
+        this.slide = 0;
+        this.index = 0;
+        this.wait = 0;
+        this.text = `I have a fear of growing up. A fear of <1000>being<150>\nimprissoned<1500> by social expectation, <1000>losing my freedom.`; 
+        this.slide2 = [`when I'm older<1000>`,`when I grow up<1000>`,`when I get a job<1000>`, `in the<1000>`,"Real World<750>"];
 
     }
     update() {
-
+        if(this.slide == 1){
+            let text = this.printText(this.out, this.text, this.slidethree, null, this);
+            this.text = text
+        }
+        if (this.slide == 2){
+            let text = this.printText()
+        }
     }
 
 }
